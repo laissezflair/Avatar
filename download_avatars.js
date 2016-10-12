dc90f4ae3c187321aaab8eaac33f1310f41bd3e3
Adam Smith <prokosch@gmail.com>
2016-10-12 19:50:26 +0000

    Refactored code, cleaned up comments, acquired avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,15 @@
+// Resolve necessary libraries (request, http, fs)
+
 var request = require('request');
 var http = require('http');
 var fs = require('fs');
 
 var sourceArgs = process.argv.slice(2);
 
-console.log(sourceArgs);
+// Confirm the arguments are properly returned without Node etc header info
+// console.log(sourceArgs);
+
+/* Define an Options variable that cites the correct API format and supplies a User-Agent ID */
 
 var options = {
       url: "https://api.github.com/repos/" + sourceArgs[0] + "/" + sourceArgs[1] + "/contributors",
@@ -13,47 +18,28 @@ var options = {
       }
     };
 
-/* Read data from GitHub contributors API */
+    /* Read data from GitHub contributors API */
 
-function getRepoContributors(repoOwner, repoName, _callback) {
-  request.get(options, function(err, data) {
-    var contribUrls = JSON.parse(data.body);
-    for (var i = 0; i < contribUrls.length; i++){
-      downloadImageByURL(contribUrls[i]["avatar_url"], "avatars/" + contribUrls[i]["login"] + ".png");
+    function getRepoContributors(repoOwner, repoName, _callback) {
+      request.get(options, function(err, data) {
+        var contribUrls = JSON.parse(data.body);
+        for (var i = 0; i < contribUrls.length; i++){
+          downloadImageByURL(contribUrls[i]["avatar_url"], "avatars/" + contribUrls[i]["login"] + ".png");
+        }
+      });
     }
-  });
-}
 
-/* Download PNG images by URL to local folder */
+    /* Download PNG images by URL to local folder // Request module to download PNG files */
 
-function downloadImageByURL(url, filePath) {
-  request(url).pipe(fs.createWriteStream(filePath)).on('close', function() {
-    return ""
+    function downloadImageByURL(url, filePath) {
+      request(url).pipe(fs.createWriteStream(filePath)).on('close', function() {
+        return ""
+      });
+    }
+
+    /* Test runner code */
+
+    getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
+      console.log("Errors:", err);
+      console.log("Result:", result);
     });
-  }
-
-
-
-/* Request module to download PNG files */
-// Need to iterate through the array and access the OBJECTS within the ARRAY
-
-// var download = function(uri, filePath, _callback){
-//   request.head(uri, function(err, res, body){
-//     // console.log('content-type:', res.headers['content-type']);
-//     // console.log('content-length:', res.headers['content-length']);
-
-//     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-//   });
-// };
-
-// download.avatar_url, function(){
-//   console.log('done');
-// };
-
-
-/* Test runner code */
-
-  getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-  });
